bbedd254cf4317940a7f73339997cf31b3665020
Ritesh <ritesh.nus@gmail.com>
2018-06-19 01:48:36 +0200

    added app error handler

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * @Author Ritesh
  * @Date 6/16/2018
- * @Description
+ * @Description: Startup file
  */
 
 var express = require('express');
@@ -17,21 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/docs', express.static(path.join(__dirname, '/api/swagger/')));
-/*// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.sendFile(path.join(__dirname+'/views/error.html'));
-});*/
 
 app.use(function(err, req, res, next) {
     console.error(err.message);
